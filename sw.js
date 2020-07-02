c4bafb4b5647bfaa1632178dec270d5f90d9d078
CompG33k <N.J.Velasco@sbcglobal.net>
2020-07-02 13:40:04 -0700

    Updates

--- sw.js
+++ sw.js
@@ -59,7 +59,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "7/2/2020, 1:29:31 PM",
+  "version": "7/2/2020, 1:40:02 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
