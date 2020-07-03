8036866d60f5e8fdef12dafcc5baccec7ad10037
CompG33k <N.J.Velasco@sbcglobal.net>
2020-07-03 02:36:05 -0700

    Updates

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ var __wpo = {
       "/2.d109a67ff5a23d712108.chunk.js",
       "/npm.redux-saga.d5d3e813629d2ee75ed4.chunk.js",
       "/npm.webpack.930eb35737a1d76dd1dc.chunk.js",
-      "/main.1333453122612dbf9498.chunk.js",
+      "/main.012eee62adcccbb103b9.chunk.js",
       "/npm.babel.cbd622b2e6a0ad546b0f.chunk.js",
       "/npm.connected-react-router.4c4f072dc44cd33b1292.chunk.js",
       "/npm.intl-messageformat.ef4553732cb09ea7cdfb.chunk.js",
@@ -39,7 +39,7 @@ var __wpo = {
     "285e7255b289cc3290a6be8b087d29fce225fa67": "/2.d109a67ff5a23d712108.chunk.js",
     "bc46e846890f6ee18aaf4f6a017f78fceb97ab66": "/npm.redux-saga.d5d3e813629d2ee75ed4.chunk.js",
     "60100b24ad6aab714b77676b831d6275085c8355": "/npm.webpack.930eb35737a1d76dd1dc.chunk.js",
-    "9ad732f5c843638fa725ecded76b19761384af98": "/main.1333453122612dbf9498.chunk.js",
+    "8de2093e535a434bc0cd79e50208e9c031c5efa4": "/main.012eee62adcccbb103b9.chunk.js",
     "a4dbe829bf0ba3f39c0bdcd4989bad8dc87da714": "/npm.babel.cbd622b2e6a0ad546b0f.chunk.js",
     "331c9621ba80bfb485560bced5fd1aff647c03dd": "/npm.connected-react-router.4c4f072dc44cd33b1292.chunk.js",
     "c7bd9d7dae0a7f2c4ddfe0e90721e396f9467b0c": "/npm.intl-messageformat.ef4553732cb09ea7cdfb.chunk.js",
@@ -55,11 +55,11 @@ var __wpo = {
     "608ede8ab06676f518c8280202c269fa3dc3038c": "/18.270ce4034a6062e548e3.chunk.js",
     "0af7c951274c3c02cd73cf35646001f3c517beff": "/19.1e073e669f7a20011b6a.chunk.js",
     "1ef76c8321632188c86874cbcf8028e412e330a0": "/20.13f7783f642aaa0ed817.chunk.js",
-    "97737dab4227ce8a5950bc481f737ee51be5dc5d": "/"
+    "f64fb0ab2eed112997697be8858213449226d993": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "7/3/2020, 2:27:39 AM",
+  "version": "7/3/2020, 2:36:03 AM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
