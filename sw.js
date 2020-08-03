468185551513f679c131373c7125fff12b4f1bb3
CompG33k <N.J.Velasco@sbcglobal.net>
2020-08-03 01:55:45 -0700

    Updates

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ var __wpo = {
     "main": [
       "/favicon.ico",
       "/61ac2421c4691dfcd926f9b964d97990.jpg",
-      "/runtime.b8f1a98b1679bcbd939f.js",
+      "/runtime.3f46f966bb5908d6daea.js",
       "/"
     ],
     "additional": [
@@ -29,7 +29,7 @@ var __wpo = {
       "/20.9cfe4e11c787db32a06a.chunk.js",
       "/21.7a91084e0dd12084bc22.chunk.js",
       "/22.6ccb0e22c37ac1c43fbd.chunk.js",
-      "/23.86f547e573d5df99f412.chunk.js",
+      "/23.4b3de3ddd028ef782ecd.chunk.js",
       "/24.d97a13a928829c0e41aa.chunk.js"
     ],
     "optional": []
@@ -55,19 +55,19 @@ var __wpo = {
     "edf0540cf7cc42d4839ad05e6518d7a576cddcde": "/npm.react-app-polyfill.92985388bacdff474f78.chunk.js",
     "35820ce2fc46172a24f5ef6577eb6849816a61a5": "/npm.react-player.7722a8e9a31259bc17aa.chunk.js",
     "acc193331757ba09ce832a5231db7c495912bdcb": "/npm.react-redux.28d878b1f361af801fa3.chunk.js",
-    "fd50d07fd35ee2f2780b58473c0a6c29c7ce4f13": "/runtime.b8f1a98b1679bcbd939f.js",
+    "dc2aa701a89d617a3faabcb3dd459eaf27707b1a": "/runtime.3f46f966bb5908d6daea.js",
     "21c11f1fdc14a258844d01972cfa688319a5913c": "/18.02100fd809e657ad4961.chunk.js",
     "2a16da267a6acbba6dc7fc2d93fa8c2c3a72abf1": "/19.6eea0c669c1da57ea4ba.chunk.js",
     "c09d134ea368c17ce8b534ce11ff6f059e69e668": "/20.9cfe4e11c787db32a06a.chunk.js",
     "0baac4ca95619cbde929669bded6f9dc8cd51881": "/21.7a91084e0dd12084bc22.chunk.js",
     "d79af37477669bf8dcde9ced4e244ef38c1c7b69": "/22.6ccb0e22c37ac1c43fbd.chunk.js",
-    "855b559a071afdb18e9a9859f0cf674047e198bb": "/23.86f547e573d5df99f412.chunk.js",
+    "83d4117520c260e93aa657efc33d99f5c4849dde": "/23.4b3de3ddd028ef782ecd.chunk.js",
     "23c4727d850dbf1a72cbef1f4b7ea5d76cdd0615": "/24.d97a13a928829c0e41aa.chunk.js",
-    "c7466de0262e864af34094a779ee58fd412722a1": "/"
+    "9eda280b490d6f9269086a18070385c9b053de7c": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "8/3/2020, 1:53:28 AM",
+  "version": "8/3/2020, 1:55:43 AM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
